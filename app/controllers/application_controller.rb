70332ef9cd20be9a415385ae2c9ce273cc1b442d
Michael Dorrance <dorrancemd@gmail.com>
2015-07-28 10:19:10 -0600

    adds logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    User.find(session[:user_id])
+    User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method : current_user
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
+  get "/logout", to: "sessions#destroy"
   root to: "welcome#index"
 end
