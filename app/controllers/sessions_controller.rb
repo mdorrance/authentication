7d27936f7fb4a317e9cfb7fd19377f31045de96a
Michael Dorrance <dorrancemd@gmail.com>
2015-07-28 11:06:20 -0600

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,17 @@ class SessionsController < ApplicationController
   end
 
   def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "You messed uup!"
+      render :new
+    end
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get "/logout", to: "sessions#destroy"
   get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   root to: "welcome#index"
 end
